0f5249a4835e8a3556a928a1c7ee4d57eeeeafb1
Fiddich <hiws99@naver.com>
2025-07-10 23:32:16 +0900

    init

---
+++ CIDI-Test/src/main/java/com/example/CIDI_Test/CidiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.CIDI_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidiTestApplication.class, args);
+	}
+
+}
---
+++ CIDI-Test/src/test/java/com/example/CIDI_Test/CidiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CIDI_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
